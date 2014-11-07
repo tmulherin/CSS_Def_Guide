48c62982c45509d80199bc126e377b16ac9f8bae
tmulher <tmulher@bu.edu>
2014-11-07 09:20:01 -0500

    Intial Commit

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("CCC_Def_Guide")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Boston University")> 
+<Assembly: AssemblyProduct("CCC_Def_Guide")> 
+<Assembly: AssemblyCopyright("Copyright © Boston University 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("39f623f5-f5d4-4b2d-b9b9-d3a4ab285455")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
